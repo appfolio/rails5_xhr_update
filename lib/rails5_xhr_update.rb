cde8a1325bb1fa19ca87ea51a95cc3eabbea4e74
Bryce Boe <bbzbryce@gmail.com>
2018-05-25 15:33:59 -0700

    Move Rails5XhrUpdate::Cli to its own file

--- lib/rails5_xhr_update.rb
+++ lib/rails5_xhr_update.rb
@@ -1,47 +1,12 @@
 # frozen_string_literal: true
 
-require 'optparse'
 require 'unparser'
 
-# Provide the Rails5XhrUpdate module including its command-line tool.
+require 'rails5_xhr_update/cli'
+
+# Provide the XHRToRails5 class.
 module Rails5XhrUpdate
   AST_TRUE = Parser::AST::Node.new(:true) # rubocop:disable Lint/BooleanSymbol)
-
-  # Provide the entry point to this program.
-  class Cli
-    def output(source, path)
-      if @options[:write]
-        File.open(path, 'w') do |file|
-          file.write(source)
-        end
-      else
-        puts source
-      end
-    end
-
-    def parse_options
-      @options = {}
-      OptionParser.new do |config|
-        config.banner = 'Usage: rails5_update.rb [options] FILE...'
-        config.on('-w', '--write', 'Write changes back to files') do |write|
-          @options[:write] = write
-        end
-      end.parse!
-    end
-
-    def run
-      parse_options
-      ARGV.each do |path|
-        buffer = Parser::Source::Buffer.new(path)
-        buffer.read
-        new_source = XHRToRails5.new.rewrite(
-          buffer, Parser::CurrentRuby.new.parse(buffer)
-        )
-        output(new_source, path)
-      end
-      0
-    end
-  end
 
   # Convert uses of the xhr method to use the rails 5 syntax.
   #
---
+++ lib/rails5_xhr_update/cli.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+module Rails5XhrUpdate
+  # Provide the entry point to this program.
+  class Cli
+    def output(source, path)
+      if @options[:write]
+        File.open(path, 'w') do |file|
+          file.write(source)
+        end
+      else
+        puts source
+      end
+    end
+
+    def parse_options
+      @options = {}
+      OptionParser.new do |config|
+        config.banner = 'Usage: rails5_update.rb [options] FILE...'
+        config.on('-w', '--write', 'Write changes back to files') do |write|
+          @options[:write] = write
+        end
+      end.parse!
+    end
+
+    def run
+      parse_options
+      ARGV.each do |path|
+        buffer = Parser::Source::Buffer.new(path)
+        buffer.read
+        new_source = XHRToRails5.new.rewrite(
+          buffer, Parser::CurrentRuby.new.parse(buffer)
+        )
+        output(new_source, path)
+      end
+      0
+    end
+  end
+end
